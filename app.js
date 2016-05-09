493fd1ebfe95fb047f7ade62190ac36ea1f50a06
OIC <oic@OICs-MacBook-Air.local>
2016-05-09 08:57:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var a = 2;
+
+var b = 8;
+
+var results = a + b;
+
+console.log(" This is where the answer should be.")
+
+var a = "Park";
+
+var b = "Way";
+
+var results = a + b;
+	console.log(" this make Parkway, Adding strings this is concatination.")
